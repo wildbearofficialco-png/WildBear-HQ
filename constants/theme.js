62e048e49140b69a5cd90290b588e6ba7c124680
wildbearofficialco-png <wildbear.official.co@gmail.com>
2026-05-13 12:44:43 -0700

    Extract theme and app constants

---
+++ constants/theme.js
@@ -0,0 +1,33 @@
+export const COLORS = {
+  bg: '#061316',
+  panel: '#0B1B22',
+  card: '#10262E',
+  card2: '#14323A',
+  border: '#254853',
+  text: '#F4FBFC',
+  muted: '#9AB3B8',
+  teal: '#52E0D4',
+  orange: '#FF9A3D',
+  green: '#71E083',
+  red: '#FF6B6B',
+  yellow: '#FFD166',
+};
+
+export const TABS = [
+  'HQ',
+  'Create',
+  'Pipeline',
+  'Products',
+  'Agents',
+  'Money',
+];
+
+export const PIPELINE_STAGES = [
+  'Idea',
+  'Script',
+  'Record',
+  'Edit',
+  'Clip',
+  'Post',
+  'Offer',
+];
